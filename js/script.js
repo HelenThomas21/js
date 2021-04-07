c94cee231397b3054a003e7c1c0b3612c54f1eef
Helen <helentomas@mail.ru>
2021-04-07 12:18:55 +0300

    22

--- js/script.js
+++ js/script.js
@@ -2,5 +2,21 @@ let score;
 let num='0';
 const firstQuestion= {
     question: "JS создан в каком году?",
-    answer: "в 1995"
+    answer: "в 1985",
+    currectAnswer: "в 1995"
+}
+const secondQuestion ={
+    question: "Создатель JS",
+    answer: "Тим Бернерс-Ли",
+    currectAnswer: "Брендан Эйх"
+}
+const thirdQuestion= {
+    question: "Имеет ли JS кроссбраузерность?",
+    answer:  "Нет",
+    currectAnswer: "Да"
+}
+const fourthQuestion= {
+    question: "Можно ли использовать подчеркивание в названиях переменных?",
+    answer: "Нет",
+    currectAnswer: "Да"
 }
